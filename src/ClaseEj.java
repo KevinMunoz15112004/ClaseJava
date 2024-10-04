d22f7c02a1b84ba9e5892f4c618854772b3f6ef1
KevinMunoz15112004 <kevinmunoz57898@gmail.com>
2024-10-04 11:55:05 +0200

    Nueva version

--- src/ClaseEj.java
+++ src/ClaseEj.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class ClaseEj {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Hola Mundo");
     }
 }
